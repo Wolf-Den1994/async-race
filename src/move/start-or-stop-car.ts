fe800f39074003af2805f87c962181652a1d8478
Denis <wolf-den@tut.by>
2021-06-13 17:51:25 +0300

    fix: btn start enable after geting error 500

--- src/move/start-or-stop-car.ts
+++ src/move/start-or-stop-car.ts
@@ -82,6 +82,7 @@ export const startCar = async function startCarFromButton(
 
     const secondAnswer = await drive(id, StatusCar.Drive);
     if (!secondAnswer) {
+      button.disabled = false;
       raceObj.numCarsRunning++;
       window.cancelAnimationFrame(raceObj.idAnimation[id]);
     }
